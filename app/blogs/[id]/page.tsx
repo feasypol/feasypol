a89c343a6a032ca82fe49d0c33865363b0b3a1df
easy <feasypol@gmail.com>
2024-09-15 21:05:12 +0700

    robots.txt & blog

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -2,10 +2,8 @@ import Nav from "@/app/component/navbar/Nav";
 import blog from "../../blogs.json";
 import Footer from "@/app/component/footer/Footer";
 import { metadata } from "@/app/layout";
-import dynamic from 'next/dynamic';
 import Copyright from "@/app/component/Copyright/copyright";
-
-const ImageWeb = dynamic(() => import("@/app/component/Imageweb/Imageweb"), { ssr: false });
+import Image from "next/image";
 
 export default function Page(params: {params: {id: string}}) {
     const id = params.params.id;
@@ -13,22 +11,27 @@ export default function Page(params: {params: {id: string}}) {
     metadata.title = blogPost?.title + " - feasypol";
 
     return (
-      <div className="p-10 lg:px-64 flex w-full flex-col text-xl">
+      <div className="p-8 lg:px-64 flex w-full flex-col text-xl lg:text-2xl">
         <Nav />
         <h1 className="text-center self-center underline font-bold mt-4">
           {blogPost?.title}
         </h1>
         <p className="self-center mt-2 mb-8">{blogPost?.date}</p>
-        <div className="flex w-full justify-center mb-8">
-        <ImageWeb
-            srcDesktop="/assets/cover_desktop.png"
-            srcMobile="/assets/cover_mobile.png"
-            alt="placeholder"
-            widthDesktop={1500}
-            heightDesktop={300}
-            widthMobile={500}
-            heightMobile={100}
+        <div className="w-full lg:w-2/3 self-center mb-8">
+        {blogPost?.image && (
+          <Image
+            src={blogPost.image}
+            alt={blogPost.imageAlt}
+            title={blogPost.imageAlt}
+            width={1000}
+            height={750}
           />
+        )}
+          {blogPost?.imageCaption && (
+            <p className="text-sm text-gray-500 mt-2 text-center">
+              {blogPost.imageCaption}
+            </p>
+        )}
         </div>
         {blogPost?.content.map((content: string, index) => (
           <div key={index} >
@@ -36,7 +39,6 @@ export default function Page(params: {params: {id: string}}) {
             <br />
           </div>
         ))}
-
         <Footer />
         <Copyright />
       </div>
